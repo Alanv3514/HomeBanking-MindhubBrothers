d10349c5ff993d9141a9bb7a405bd7393e8b34ed
Alanv3514 <alanv3514@gmail.com>
2023-08-08 20:22:41 -0300

    Add ID Add Description Add Date Add getters

--- src/main/java/com/mindhub/HomeBanking/dtos/TransactionDto.java
+++ src/main/java/com/mindhub/HomeBanking/dtos/TransactionDto.java
@@ -4,19 +4,43 @@ import com.mindhub.HomeBanking.enums.TransactionType;
 import com.mindhub.HomeBanking.models.Account;
 import com.mindhub.HomeBanking.models.Transaction;
 
+import java.time.LocalDate;
+
 
 public class TransactionDto {
 
     private Long id;
     private TransactionType type;
     private Double amount;
-
+    private LocalDate date;
+    private String description;
     public TransactionDto(){};
 
     public TransactionDto(Transaction transaction) {
+        this.id = transaction.getId();
         this.type = transaction.getType();
         this.amount = transaction.getAmount();
+        this.date = transaction.getDate();
+        this.description = transaction.getDescription();
     }
 
+    public Long getId() {
+        return id;
+    }
 
+    public TransactionType getType() {
+        return type;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
