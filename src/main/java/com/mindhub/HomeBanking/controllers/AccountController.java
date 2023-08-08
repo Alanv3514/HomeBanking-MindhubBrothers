d48f41d7428bd173312e31ce07f0a847ad5bbfa8
Alanv3514 <alanv3514@gmail.com>
2023-08-08 20:17:03 -0300

    Add Account Controller

---
+++ src/main/java/com/mindhub/HomeBanking/controllers/AccountController.java
@@ -0,0 +1,30 @@
+package com.mindhub.HomeBanking.controllers;
+
+import com.mindhub.HomeBanking.dtos.AccountDto;
+import com.mindhub.HomeBanking.repositories.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import static java.util.stream.Collectors.toList;
+
+@RestController
+@RequestMapping("/api")
+public class AccountController {
+
+    @Autowired
+    private AccountRepository accountRepository;
+    @RequestMapping("/accounts")
+    public List<AccountDto> getAll(){
+        return accountRepository.findAll().stream()
+                .map(account -> new AccountDto(account))
+                .collect(toList());
+    }
+    @RequestMapping("/accounts/{id}")
+    public AccountDto getById(@PathVariable Long id){
+        return new AccountDto(accountRepository.findById(id).orElse(null));
+    }
+
+}
