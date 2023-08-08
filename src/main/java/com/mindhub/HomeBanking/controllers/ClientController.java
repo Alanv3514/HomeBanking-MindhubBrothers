ed3210aca34f14c952ab39ec05a39f219acee459
alan vinzon <alanv3514@gmail.com>
2023-08-08 09:54:49 -0300

    Adding /clients and /clients/{id} endpoints to ClientRestController

--- src/main/java/com/mindhub/HomeBanking/controllers/ClientController.java
+++ src/main/java/com/mindhub/HomeBanking/controllers/ClientController.java
@@ -1,12 +1,18 @@
 package com.mindhub.HomeBanking.controllers;
 
+import com.mindhub.HomeBanking.dtos.ClientDto;
 import com.mindhub.HomeBanking.models.Client;
 import com.mindhub.HomeBanking.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 @RestController
 @RequestMapping("/api")
@@ -14,10 +20,15 @@ public class ClientController {
 
     @Autowired
     private ClientRepository clientRepository;
-    @RequestMapping("/clients")
-    public List<Client> getAll(){
-        return clientRepository.findAll();
+    @RequestMapping(value = "/clients",method = RequestMethod.GET)
+    public List<ClientDto> getAll(){
+        return clientRepository.findAll().stream()
+                .map(client -> new ClientDto(client))
+                .collect(toList());
     }
-
+    @RequestMapping(value = "/clients/{id}",method = RequestMethod.GET)
+    public ClientDto getById(@PathVariable Long id){
+        return new ClientDto(clientRepository.findById(id).orElse(null));
+    }
 
 }
