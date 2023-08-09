be372ac9dfcc1f41a415030ed9a6ee250c36d401
Alanv3514 <alanv3514@gmail.com>
2023-08-09 20:47:33 -0300

    +Add Loan

---
+++ src/main/java/com/mindhub/HomeBanking/models/Loan.java
@@ -0,0 +1,58 @@
+package com.mindhub.HomeBanking.models;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Loan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private Long id;
+    private String name;
+    private Double maxAmount;
+    @ElementCollection
+    private List<Integer> payments = new ArrayList<>();
+
+    public Loan(){
+    }
+
+    public Loan(String name, double maxAmount, List<Integer> payments) {
+        this.name = name;
+        this.maxAmount = maxAmount;
+        this.payments = payments;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getMaxAmount() {
+        return maxAmount;
+    }
+
+    public void setMaxAmount(double maxAmount) {
+        this.maxAmount = maxAmount;
+    }
+
+    public List<Integer> getPayments(){
+        return payments;
+    }
+
+    public void setPayments(List<Integer> payments) {
+        this.payments = payments;
+    }
+
+}
