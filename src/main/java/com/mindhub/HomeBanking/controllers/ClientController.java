af0ecb1c56acedcd62738c3cb76b257edef410fa
Alanv3514 <alanv3514@gmail.com>
2023-08-22 20:54:57 -0300

    +Add /Clients POST to register new clients +Add /clients/current to get current autenticated client info.

--- src/main/java/com/mindhub/HomeBanking/controllers/ClientController.java
+++ src/main/java/com/mindhub/HomeBanking/controllers/ClientController.java
@@ -1,11 +1,14 @@
 package com.mindhub.HomeBanking.controllers;
 
 import com.mindhub.HomeBanking.models.Dtos.ClientDto;
+import com.mindhub.HomeBanking.models.Entities.Client;
 import com.mindhub.HomeBanking.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,7 +17,8 @@ import static java.util.stream.Collectors.toList;
 @RestController
 @RequestMapping("/api")
 public class ClientController {
-
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     @Autowired
     private ClientRepository clientRepository;
     @RequestMapping("/clients")
@@ -23,9 +27,45 @@ public class ClientController {
                 .map(client -> new ClientDto(client))
                 .collect(toList());
     }
+
+    @RequestMapping(path = "/clients", method = RequestMethod.POST)
+
+    public ResponseEntity<Object> register(
+
+            @RequestParam String firstName, @RequestParam String lastName,
+
+            @RequestParam String email, @RequestParam String password) {
+
+
+        if (firstName.isEmpty() || lastName.isEmpty() || email.isEmpty() || password.isEmpty()) {
+
+            return new ResponseEntity<>("Missing data", HttpStatus.FORBIDDEN);
+
+        }
+
+
+        if (clientRepository.findByEmail(email) != null) {
+
+            return new ResponseEntity<>("Name already in use", HttpStatus.FORBIDDEN);
+
+        }
+
+
+        clientRepository.save(new Client(firstName, lastName, email, passwordEncoder.encode(password)));
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
+
+    }
+
     @RequestMapping("/clients/{id}")
     public ClientDto getById(@PathVariable Long id){
         return new ClientDto(clientRepository.findById(id).orElse(null));
     }
+
+    @RequestMapping("/clients/current")
+    public ClientDto getCurrentClient(Authentication authentication) {
+        return new ClientDto(clientRepository.findByEmail(authentication.getName()));
+    }
+
 
 }
