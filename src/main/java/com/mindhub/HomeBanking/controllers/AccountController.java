8dc1c38cb224f957611911bcaf656d2f58e826b1
alan vinzon <alanv3514@gmail.com>
2023-08-23 11:49:47 -0300

    +adding /clients/current/accounts

--- src/main/java/com/mindhub/HomeBanking/controllers/AccountController.java
+++ src/main/java/com/mindhub/HomeBanking/controllers/AccountController.java
@@ -1,14 +1,21 @@
 package com.mindhub.HomeBanking.controllers;
 
 import com.mindhub.HomeBanking.models.Dtos.AccountDto;
+import com.mindhub.HomeBanking.models.Dtos.ClientDto;
+import com.mindhub.HomeBanking.models.Entities.Account;
+import com.mindhub.HomeBanking.models.Entities.Client;
 import com.mindhub.HomeBanking.repositories.AccountRepository;
+import com.mindhub.HomeBanking.repositories.ClientRepository;
+import net.bytebuddy.asm.Advice;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
+
 import static java.util.stream.Collectors.toList;
 
 @RestController
@@ -17,6 +24,9 @@ public class AccountController {
 
     @Autowired
     private AccountRepository accountRepository;
+    @Autowired
+    private ClientRepository clientRepository;
+
     @RequestMapping("/accounts")
     public List<AccountDto> getAll(){
         return accountRepository.findAll().stream()
@@ -31,4 +41,33 @@ public class AccountController {
         return new AccountDto(accountRepository.findById(id).orElse(null));
     }
 
+    @RequestMapping(path = "/clients/current/accounts", method = RequestMethod.GET)
+
+    public List<AccountDto> getCurrentAccounts( Authentication authentication) {
+
+        return accountRepository.findAll().stream()
+                .filter(account -> account.getOwner().getEmail().equals(authentication.getName()))
+                .map(account -> new AccountDto(account))
+                .collect(toList());
+    }
+
+    @RequestMapping(path = "/clients/current/accounts", method = RequestMethod.POST)
+
+    public ResponseEntity<Object> createAccount( Authentication authentication) {
+
+
+        if (clientRepository.findByEmail(authentication.getName()).getAccounts().stream().count()==3) {
+
+            return new ResponseEntity<>("Already have 3 accounts", HttpStatus.FORBIDDEN);
+
+        }
+
+        Account newAccount= new Account("VIN"+String.format("%03d",accountRepository.count()+1) , 0.0, LocalDate.now());
+        Client AuthClient = clientRepository.findByEmail(authentication.getName());
+        AuthClient.addAccount(newAccount);
+        accountRepository.save(newAccount);
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
+
+    }
 }
