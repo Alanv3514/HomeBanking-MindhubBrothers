64f1b0e31ce2ecf9c3a192d5a33a22950e1b5921
alan vinzon <alanv3514@gmail.com>
2023-08-23 11:51:14 -0300

    update authorizeRequests

--- src/main/java/com/mindhub/HomeBanking/configurations/WebAuthorization.java
+++ src/main/java/com/mindhub/HomeBanking/configurations/WebAuthorization.java
@@ -27,8 +27,10 @@ class WebAuthorization  {
         http.authorizeRequests()
                 .antMatchers("/web/index.html","/web/js/**","/web/css/**","/web/img/**").permitAll()
                 .antMatchers("/admin/**","/rest/**").hasAuthority("ADMIN")
-                .antMatchers(HttpMethod.GET,"/api/clients/current","/web/**","/api/accounts/**").hasAuthority("USER")
+                .antMatchers(HttpMethod.GET,"/api/clients/current","/web/**","/api/accounts/**","/api/login","/api/logout","/api/clients/**").hasAuthority("USER")
+                .antMatchers(HttpMethod.POST,"/api/clients/current/**","/api/logout").hasAuthority("USER")
                 .antMatchers(HttpMethod.POST,"/api/**").permitAll()
+                .antMatchers("/h2-console/**").permitAll()
                 .anyRequest().denyAll();
 
 
