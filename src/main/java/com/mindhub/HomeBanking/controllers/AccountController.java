90cee605f63e67f588b948da8a0a94b105e64c7d
Alanv3514 <alanv3514@gmail.com>
2023-08-22 21:59:30 -0300

    +Add authentication check

--- src/main/java/com/mindhub/HomeBanking/controllers/AccountController.java
+++ src/main/java/com/mindhub/HomeBanking/controllers/AccountController.java
@@ -3,6 +3,7 @@ package com.mindhub.HomeBanking.controllers;
 import com.mindhub.HomeBanking.models.Dtos.AccountDto;
 import com.mindhub.HomeBanking.repositories.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +24,10 @@ public class AccountController {
                 .collect(toList());
     }
     @RequestMapping("/accounts/{id}")
-    public AccountDto getById(@PathVariable Long id){
+    public AccountDto getById(@PathVariable Long id, Authentication authentication){
+        if(!accountRepository.findById(id).get().getOwner().getEmail().equals(authentication.getName())){
+            return null;
+        }
         return new AccountDto(accountRepository.findById(id).orElse(null));
     }
 
