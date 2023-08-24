5ef80b85f87c9ccb69ffc3f97f2c4eff6c991fb0
Alanv3514 <alanv3514@gmail.com>
2023-08-23 21:40:46 -0300

    =fix utils

--- src/main/java/com/mindhub/HomeBanking/models/entities/Card.java
+++ src/main/java/com/mindhub/HomeBanking/models/entities/Card.java
@@ -8,6 +8,8 @@ import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.Random;
 
+import static com.mindhub.HomeBanking.utils.utils.*;
+
 @Entity
 public class Card {
     @Id
@@ -34,8 +36,8 @@ public class Card {
     public Card( CardType type, CardColor color, LocalDate fromDate) {
         this.type = type;
         this.color = color;
-        setNumber();
-        setCvv(this.number);
+        setNumber(genRandomCardNumber());
+        setCvv(genCvv(this.number));
         this.fromDate = fromDate;
         setThruDate(fromDate);
     }
@@ -68,29 +70,18 @@ public class Card {
         return number;
     }
 
-    public void setNumber() {
-        String cardNumber = "8545";
-        Random random = new Random();
-        for (int i = 0; i < 3; i++) {
-            cardNumber += "-" + String.format("%04d", random.nextInt(10000));
-        }
-        this.number = cardNumber;
+    public void setNumber(String number) {
+        this.number = number;
     }
+
     public Integer getCvv() {
         return cvv;
     }
 
-    public void setCvv(String cardNumber) {
-        int sum = 0;
-        for (String bloque : cardNumber.split("-")) {
-            int aux = 0;
-            for (int i = 0; i < 3; i++) {
-                aux += Character.getNumericValue(bloque.charAt(i));
-            }
-            sum += aux;
-        }
-        this.cvv= 300+(sum % 10000);
+    public void setCvv(Integer cvv) {
+        this.cvv = cvv;
     }
+
     public LocalDate getFromDate() {
         return fromDate;
     }
--- src/main/java/com/mindhub/HomeBanking/utils/utils.java
+++ src/main/java/com/mindhub/HomeBanking/utils/utils.java
@@ -6,8 +6,25 @@ import java.util.Random;
 
 public class utils {
 
-
-
+    public static String genRandomCardNumber() {
+        String cardNumber = "8545";
+        Random random = new Random();
+        for (int i = 0; i < 3; i++) {
+            cardNumber += "-" + String.format("%04d", random.nextInt(10000));
+        }
+        return cardNumber;
+    }
+    public static Integer genCvv(String cardNumber) {
+        int sum = 0;
+        for (String bloque : cardNumber.split("-")) {
+            int aux = 0;
+            for (int i = 0; i < 3; i++) {
+                aux += Character.getNumericValue(bloque.charAt(i));
+            }
+            sum += aux;
+        }
+        return (300+(sum % 10000));
+    }
 
     public static String genAccountId(AccountRepository accountRepository) {
         return "VIN"+String.format("%03d",accountRepository.count()+1 );
