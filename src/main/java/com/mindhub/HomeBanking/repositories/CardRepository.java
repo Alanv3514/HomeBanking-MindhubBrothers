c7f9026943ebe5cd9a2e9ce97e86a978d087f06c
Alanv3514 <alanv3514@gmail.com>
2023-08-30 18:58:24 -0300

    +Add findByOwner to CardRepository

--- src/main/java/com/mindhub/HomeBanking/repositories/CardRepository.java
+++ src/main/java/com/mindhub/HomeBanking/repositories/CardRepository.java
@@ -1,11 +1,15 @@
 package com.mindhub.HomeBanking.repositories;
 
 import com.mindhub.HomeBanking.models.entities.Card;
+import com.mindhub.HomeBanking.models.entities.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import java.util.ArrayList;
 
 @RepositoryRestResource
 public interface CardRepository extends JpaRepository<Card, Long> {
     //findByNumber
     Card findByNumber(String number);
+
+    ArrayList<Card> findByOwner(Client authClient);
 }
