f6de552587d8233a97cb2e41490b6d3283608f0a
alan vinzon <alanv3514@gmail.com>
2023-08-31 10:32:27 -0300

    +Add object to recieve requestbody

---
+++ src/main/java/com/mindhub/HomeBanking/dtos/ClientLoanRecord.java
@@ -0,0 +1,47 @@
+package com.mindhub.HomeBanking.dtos;
+
+public class ClientLoanRecord {
+    private Long loanId;
+    private  Double amount;
+    private  Integer payments;
+    private  String toAccountNumber;
+
+    public ClientLoanRecord(Long loanId, Double amount, Integer payments, String toAccountNumber) {
+        this.loanId = loanId;
+        this.amount = amount;
+        this.payments = payments;
+        this.toAccountNumber = toAccountNumber;
+    }
+
+    public Long getLoanId() {
+        return loanId;
+    }
+
+    public void setLoanId(Long loanId) {
+        this.loanId = loanId;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public Integer getPayments() {
+        return payments;
+    }
+
+    public void setPayments(Integer payments) {
+        this.payments = payments;
+    }
+
+    public String getToAccountNumber() {
+        return toAccountNumber;
+    }
+
+    public void setToAccountNumber(String toAccountNumber) {
+        this.toAccountNumber = toAccountNumber;
+    }
+}
